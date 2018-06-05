8d8b2b0f4c5ede83b526bd3a8e771446da2dcedc
yuto.komasu <yuto.komatsu@rakuten.com>
2018-06-05 05:28:27 +0000

    user registration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,24 @@
-<！DOCTYPE html> 
-<head> 
-    < 
-        meta charset = "utf-8"> 
-        <meta-url-compatible> content = "IE = edge"> 
-        <meta name = "viewport" content = "width = device-width、initial-scale = 1"> 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title> Microposts </ 
+        <title>Microposts</title>
 
-        - > <！ -  Bootstrap  - > 
-        <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com /bootstrap/3.3.7/css/bootstrap.min.css "> 
-        <script src =" https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js "> </スクリプト> 
-        <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"> </ script> 
-    </ head> 
-    <body> 
-        @include（ 'commons.navbar'）
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        @include('commons.navbar')
 
-        <div class = "container">
-            @include（ 'commons.error_messages'）
+        <div class="container">
+            @include('commons.error_messages')
 
-            @yield（ 'content'）
-        </ div> 
-    </ body> 
-</ html>
+            @yield('content')
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
